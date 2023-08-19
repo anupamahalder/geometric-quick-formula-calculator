5d0db09c7ef5beb70b5138c45740b6498b11a95a
Anupama_Halder <anupamahalder2022@gmail.com>
2023-08-19 20:08:16 +0530

    Data Validation

--- calculator.js
+++ calculator.js
@@ -1,3 +1,11 @@
+/*
+    Data Validation:
+    1. set the proper type of the input field(number,data,email)
+    2. check type using typeof
+    3. isNaN means: Not a number. isNaN is checking whether the input is not a number  
+*/
+
+
 function getInputElementById(inputId){
     const inputField = document.getElementById(inputId);
     const inputValueText = inputField.value;
@@ -20,6 +28,13 @@ function triangleAreaCalculate(){
     const heightField = document.getElementById('triangle-height');
     const heightValue = getInputElementById('triangle-height');
 
+    //Data validation check
+    if(isNaN(baseValue) || isNaN(heightValue)){
+        alert('Please enter a valid number!');
+        baseField.value = '';
+        heightField.value = '';    
+        return;
+    }
     //area calculate
     const area = 0.5 * baseValue * heightValue;
 
@@ -40,6 +55,14 @@ function rectangleAreaCalculate(){
     //get triangle base value
     const heightField = document.getElementById('rectangle-height');
     const heightValue = getInputElementById('rectangle-height');
+
+    //Data validation check
+    if(isNaN(baseValue) || isNaN(heightValue)){
+        alert('Please enter a valid number!');
+        baseField.value = '';
+        heightField.value = '';    
+        return;
+    }
 
     //area calculate
     const area = baseValue * heightValue;
@@ -63,6 +86,14 @@ function parallelogramAreaCalculate(){
     const heightField = document.getElementById('parallelogram-height');
     const heightValue = getInputElementById('parallelogram-height');
 
+    //Data validation check
+    if(isNaN(baseValue) || isNaN(heightValue)){
+        alert('Please enter a valid number!');
+        baseField.value = '';
+        heightField.value = '';    
+        return;
+    }
+
     //area calculate
     const area = baseValue * heightValue;
 
@@ -85,6 +116,14 @@ function ellipseAreaCalculate(){
     //get triangle base value
     const heightField = document.getElementById('ellipse-radius2');
     const heightValue = getInputElementById('ellipse-radius2');
+
+    //Data validation check
+    if(isNaN(baseValue) || isNaN(heightValue)){
+        alert('Please enter a valid number!');
+        baseField.value = '';
+        heightField.value = '';    
+        return;
+    }
 
     //area calculate
     const area = Math.PI *baseValue * heightValue;
